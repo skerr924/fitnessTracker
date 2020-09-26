2dbc0a08963050fcb06d5d40fd1f3823c984792c
Sarah Kerr <sarah.kerr93@gmail.com>
2020-09-26 15:44:30 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 require("./routes/api-routes")(app);
