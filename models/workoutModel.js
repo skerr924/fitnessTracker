01826dac396e550ed8a900b6ede12ddd40d0bab2
Sarah Kerr <sarah.kerr93@gmail.com>
2020-09-26 15:36:14 -0400

    corrected JSON reference error in virtuals in model

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -41,7 +41,7 @@ const WorkoutSchema = new Schema(
   },
   {
     toObject: { virtuals: true },
-    toJson: { virtuals: true },
+    toJSON: { virtuals: true },
   }
 );
 
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  // useUnifiedTopology: true,
+  useUnifiedTopology: true,
   // useCreateIndex: true,
   // useFindAndModify: true,
 });
